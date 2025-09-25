57d77779c0abb24c19d6eaafaa3afa8ce166a424
Kanomlay <165756752+Kanomlay@users.noreply.github.com>
2025-09-25 20:35:23 +0700

    3หน้า

--- flutter_application_delivery/lib/main.dart
+++ flutter_application_delivery/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_delivery/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,9 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // เอา debug ออก
+      title: 'Swift Drop',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const LoginPage(), // หน้าแรก
+    );
   }
 }

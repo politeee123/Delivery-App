fd3c91eedb808ae421664f6aadbb545951c4dca9
Polite <147123869+politeee123@users.noreply.github.com>
2025-09-26 18:50:46 +0700

    1

--- flutter_application_delivery/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_application_delivery/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,8 @@ import cloud_firestore
 import file_selector_macos
 import firebase_core
 import firebase_storage
+import geolocator_apple
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -20,6 +22,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
